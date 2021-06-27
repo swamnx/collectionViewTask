a3c39975b9902ab8fd200ba675ac948169c62d3b
Swmnx <No>
2021-06-27 20:33:29 +0300

    Initial Commit

---
+++ collectionViewTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  collectionViewTask
+//
+//  Created by swamnx on 27.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
